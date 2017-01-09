22a7cee8b17f4164ae5a377663392510f88f878f
Navraj Narula <navrajnarula@gmail.com>
2017-01-09 19:31:02 +0700

    starting OCaml basics

---
+++ OCaml/hello.ml
@@ -0,0 +1,24 @@
+(* I'm just a girl trying to learn the basics of OCaml - learn with me! *)
+(* Much credit to for assistance: https://try.ocamlpro.com *)
+
+(* Classic print statement --> "Hello, world!"*)
+print_string "Hello, World!\n";;
+
+(* list with ints *)
+[1;2;3]
+
+(* list with strings *)
+[|"Navie";"Narula"]
+
+(* reverse list *)
+List.rev[1;2;3]
+
+(* get 1st element of list // hd = head *)
+List.hd[1;2;3]
+
+(* concatenate arrays *)
+Array.append[|1;2|][|3;4|]
+
+(* gets second, or last, element of array *)
+Array.get [|1; 2; 3|] 2
+
