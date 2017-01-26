0413d047bbf2e988996397afbcfa779897b90514
Navraj Narula <navrajnarula@gmail.com>
2017-01-25 19:17:27 -0500

    graphing with vectors

--- R/hello.r
+++ R/hello.r
@@ -6,3 +6,85 @@
 "Navie"
 2 * 3
 2 + 2 == 4
+
+# variable assignments
+x <- 42
+x <- "Navie"
+x <- TRUE
+
+# sum function
+sum(1, 2, 3)
+
+# repeat function
+rep("Bye!", times = 2)
+
+# square root
+sqrt(16)
+
+# create vector, all values inside must be SAME type
+# if not, they will be converted to same type
+c(4, 5, 6)
+c("a", "b", "c")
+
+# vector with a sequence
+# start:end
+5:9
+seq(5, 9)
+# specify range
+seq(5, 9, 0.5)
+# opposite way
+9:5
+
+# indexing, starts at 1
+sentence <- c("walk", "me", "home")
+# retrieve a range
+sentence[1:2]
+# setting a range
+sentence[1:3] <- c("take", "me", "away")
+sentence[3]
+
+# assign names to 3-item vector
+ranks <- 1:3
+names(ranks) <- c("first", "second", "third")
+# ranks["first"] = 1
+
+# bar plotting
+runawayPups <- c(4, 5, 1)
+barplot(runawayPups)
+
+# add names for pups (as labels)
+names(runawayPups) <- c("Mark", "Tom", "Travis")
+barplot(runawayPups)
+
+# plot 100 values!
+barplot(1:100)
+
+# you can add 1 to every value in the vector
+a <- c(1, 2, 3)
+a + 1
+# and divide
+a / 2
+
+# you can add two vectors together
+
+b <- c(4, 5, 6)
+a + b
+
+# comparison: TRUE FALSE FALSE
+# <, >, or ==
+a == c(1, 7, 8)
+
+# get sin of vals
+sin(a)
+
+# scatter plot, vectors x and y
+x <- seq(1, 20, 0.1)
+y <- sin(x)
+plot(x, y)
+
+values <- -10:10
+absolutes <- abs(values)
+plot(values, absolutes)
+
+
+
