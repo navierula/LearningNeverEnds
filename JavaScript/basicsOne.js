310f6532852f1bdbd87e114e4551ccad05f03a5c
Navraj Narula <navrajnarula@gmail.com>
2017-05-09 20:43:10 -0400

    add js folder

---
+++ JavaScript/basicsOne.js
@@ -0,0 +1,62 @@
+/* freeCodeCamp - Practice by Navie Narula */
+
+// This is a one-line comment
+/* This a more-than-one-line...
+comment */
+
+// variables and reassignemnt
+var name;
+var age;
+name = "Navie";
+age = 23;
+name = age;
+var sum = 10 + 10;
+var prodcut = 10 * 7;
+var quotient = 10 / 2;
+var remainder;
+remainder = 11 % 3;
+
+// increment and decrement
+var plus = 1;
+plus++;
+plus--;
+
+// compound assignment
+
+// add
+var a = 3;
+var b = 17;
+var c = 12;
+
+a = a += 12;
+b = b += 9;
+c = c += 7;
+
+// subtract
+var a = 11;
+var b = 9;
+var c = 3;
+
+a = a -= 6;
+b = b -= 15;
+c = c -= 1;
+
+// mult
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a = a *= 5;
+b = b *= 3;
+c = c *= 10;
+
+// divide
+var a = 48;
+var b = 108;
+var c = 33;
+
+a = a /= 12;
+b = b /= 4;
+c = c /= 11;
+
+
